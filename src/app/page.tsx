2b92054e97d3c4c3914c604ae7ffb0d84b6615b0
Dan Druff <hamtron2000@gmail.com>
2023-07-16 09:10:44 -0400

    added tree

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <p>MAIN PAGE</p>
+      <Link href="/threeD">3D</Link>
     </div>
   )
 }
---
+++ src/app/threeD/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { VRButton, XR } from '@react-three/xr';
+import styles from '../page.module.css';
+import { Canvas } from '@react-three/fiber';
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const ThreeDPage = () => {
+    const model = useLoader(GLTFLoader, "./models/tree1.glb");
+  return (
+    <div className={styles.container}>
+        <VRButton />
+        <Canvas shadows>
+            <XR>
+            <ambientLight intensity={0.5}/>
+            <mesh rotation-x={Math.PI * -0.5} receiveShadow>
+                <planeGeometry args={[1000,1000]} />
+                <meshStandardMaterial color="green" />
+            </mesh>
+            <object3D position={[2,0,0]}>
+                <primitive object={model.scene}></primitive>
+            </object3D>
+            </XR>
+        </Canvas>
+    </div>
+  )
+}
+
+export default ThreeDPage
